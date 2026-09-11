9d4d79b1d5637cb991b21e322f681d5cfe10ce57
Santi77-1 <arturobe211@gmail.com>
2026-09-11 18:39:33 -0500

    CreacionDB

--- Biblioteca/Biblioteca/obj/Debug/net10.0-windows/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/Biblioteca/obj/Debug/net10.0-windows/Biblioteca.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d36a3537ca223c0a88dd0793d838b013df3bf56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed508a5e9e63ad8189d72b4120685bca9a73b54c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
